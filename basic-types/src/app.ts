2dd51f3888bd10cdbfac53d457b59054974b9169
Rahat Khan <kmrejoantanjim@gmail.com>
2025-11-24 12:13:57 +0600

    array aand tuples

--- basic-types/src/app.ts
+++ basic-types/src/app.ts
@@ -1,14 +1,29 @@
-// premitive data type
+// // premitive data type
 
-// string
-let name : string = "rahat"
+// // string
+// let name : string = "rahat"
 
-name = "patel"
+// name = "patel"
 
-let age  = 23
-age = 29
+// let age  = 23
+// age = 29
 
 
-let isMarrid :boolean = false
-isMarrid=true
-console.log(name,age,isMarrid);
+// let isMarrid :boolean = false
+// isMarrid=true
+// console.log(name,age,isMarrid);
+
+// array and tuples
+let score :number[]= [23,4,6,788,54]
+const score2 :Array<number> = [23,43564,6654,23434]
+let name : string[] =["rahat","patel"]
+const name2 : Array<string> = ["rahat khn","ptel khn","gorib"]
+console.log(name[1]);
+console.log(name2[2]);
+
+console.log(score);
+console.log(score2);
+
+// tuples
+const student : [string,number,boolean]= ["rahat",26,false]
+console.log(student);
