49922e5957ead015c3adf8dc578bbe9eda3a724f
Rahat Khan <kmrejoantanjim@gmail.com>
2025-11-24 11:55:49 +0600

    data types

---
+++ basic-types/src/app.ts
@@ -0,0 +1,14 @@
+// premitive data type
+
+// string
+let name : string = "rahat"
+
+name = "patel"
+
+let age  = 23
+age = 29
+
+
+let isMarrid :boolean = false
+isMarrid=true
+console.log(name,age,isMarrid);
